6ef39f8964c00ee9f95603f49c7542cb3382eaed
Joao Figueiras <jfigueiras@factset.com>
2016-08-01 16:19:36 +0100

    Added support for a continous feed of connection request updates.

---
+++ src/SymphonyOSS.RestApiClient/Api/PodApi/ConnectionRequestEventArgs.cs
@@ -0,0 +1,18 @@
+﻿namespace SymphonyOSS.RestApiClient.Api.PodApi
+{
+    using System;
+    using Generated.OpenApi.PodApi.Model;
+
+    /// <summary>
+    /// Event data for connection requests.
+    /// </summary>
+    public class ConnectionRequestEventArgs : EventArgs
+    {
+        public ConnectionRequestEventArgs(UserConnection connection)
+        {
+            Connection = connection;
+        }
+
+        public UserConnection Connection { get; private set; }
+    }
+}
