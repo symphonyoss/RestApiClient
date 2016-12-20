86cbe8db3b6036f5518a462cecb50bae14e06cf4
Maurizio Pillitu <maoo@symphony.foundation>
2016-12-20 09:33:07 +0100

    removing placeholder as it led to build failure

--- test/SymphonyOSS.RestApiClient.Tests/Properties/AssemblyInfo.cs
+++ test/SymphonyOSS.RestApiClient.Tests/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.4.2.%BuildCounter%")]
-[assembly: AssemblyInformationalVersion("0.4.2-beta%BuildCounter%")]
+[assembly: AssemblyFileVersion("0.4.2.0")]
+[assembly: AssemblyInformationalVersion("0.4.2-beta0")]
