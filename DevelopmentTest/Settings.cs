62b9d9d19811c416dd1ec8dc5122240ee5d51e82
Malay Shah <mshah@factset.com>
2017-09-20 11:23:31 -0700

    Add DevelopmentTest application to aid with integration testing of changes. The settings will be read from a file external to the repo since we usually require a pod and certificate to do anything useful.

---
+++ DevelopmentTest/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevelopmentTest
+{
+    public class Settings
+    {
+        public string sessionauthUrl;
+        public string keyauthUrl;
+        public string agentUrl;
+
+        public string certificate;
+        public string certificatePassword;
+    }
+}
